58ef880259a38ca30b1b079b628a5e6461a8daa2
acyeh-lab <133069589+acyeh-lab@users.noreply.github.com>
2024-12-10 20:23:38 -0800

    Add files via upload

---
+++ NetMHCIIPan_Analysis.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+dir=$1 #Directory with all the .txt files ONLY
+hla=$2
+
+subdir="$1/Results"
+files="ls $1/*.txt"
+mkdir $dir/$hla
+
+for eachfile in $files
+do
+  temp=$(basename $eachfile)
+  subfile="$dir/$hla/$temp"
+  /fh/fast/hill_g/Albert/NetMHCIIPan/netMHCIIpan-4.1/netMHCIIpan "-f" $eachfile "-a" $hla > $subfile
+done
+
+#A=$1 #File to Anlayze without the ".txt"
+#B=$2 #HLA subtype
+#C="$1.txt"
+#D="$1_analysis.txt"
+
+#/fh/fast/hill_g/Albert/NetMHCIIPan/netMHCIIpan-4.1/netMHCIIpan "-f" $C "-a" $B > $D
+
